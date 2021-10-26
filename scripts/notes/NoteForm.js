dbfa27039963edf723d971fbb5073788d04be678
cbgriffith <c.b.griffith@outlook.com>
2021-10-26 14:12:42 -0400

    finally got it working

--- scripts/notes/NoteForm.js
+++ scripts/notes/NoteForm.js
@@ -1,37 +1,39 @@
+import { NoteList } from "./NoteList.js"
 import { saveNote } from "./NoteDataProvider.js"
 
 const contentTarget = document.querySelector(".noteFormContainer")
 
 // Handle browser-generated click event in component
-// eventHub.addEventListener("click", clickEvent => {
-//   if (clickEvent.target.id === "saveNote") {
+contentTarget.addEventListener("click", clickEvent => {
+  if (clickEvent.target.id === "saveNote") {
 
-//       // Make a new object representation of a note
-//       const newNote = {
+      // Make a new object representation of a note
+      const newNote = {
+         criminalName: document.querySelector('#note__criminalName').value,
+         date: document.querySelector('#note__date').value,
+         text: document.querySelector('#note__text').value
+      }
 
-//       }
-
-//       // Change API state and application state
-//       saveNote(newNote)
-//       .then(NoteList) // Refresh your list of notes once you've saved your new one
-//   }
-// })
-
+      // Change API state and application state
+      saveNote(newNote)
+      .then(NoteList) // Refresh your list of notes once you've saved your new one
+  }
+})
 
 export const NoteForm = () => {
     contentTarget.innerHTML = `<form>
     <div>
       <label>Suspect Name:</label>
     </div>
-      <input type="text" id="note-suspect" required>
+      <input type="text" id="note__criminalName" required>
     <div>
       <label>Date:</label>
     </div>
-      <input type="date" id="note-date" required>
+      <input type="date" id="note__date" required>
     <div>
       <label>Note:</label>
     </div>
-      <textarea id="note-text"></textarea required>
+      <textarea id="note__text"></textarea required>
     <div>
       <button id="saveNote" class="btn btn-primary">Save</button>
     </div>
